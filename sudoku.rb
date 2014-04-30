feb90b1ffb4c5f77756fd41bcb9e05df6bed7cf7
Robin <robindoble@gmai.com>
2014-04-30 14:07:02 +0100

    now reading a random sudoku grid and displaying in the browser

--- sudoku.rb
+++ sudoku.rb
@@ -1,8 +1,18 @@
 require 'sinatra'
+require_relative './lib/sudoku'
+require_relative './lib/cell'
+	
+def random_sudoku
+		seed =(1..9).to_a.shuffle + Array.new(81-9,0)
+		sudoku=Sudoku.new(seed.join)
+		sudoku.solve!
+		sudoku.to_s.chars
+end
+
+# puts random_sudoku.inspect
 
 get '/' do 
-
-'Hello, Sudoku'
-
+	@current_solution = random_sudoku
+	erb :index
 end
 
